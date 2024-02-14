506f0c3e795f5acd68612b2a18412a170af4ad20
TakiDaRAR <159424586+TakiDaRAR@users.noreply.github.com>
2024-02-14 16:25:11 +0300

    Create zadanie5.py

---
+++ zadanie5.py
@@ -0,0 +1,19 @@
+def complex(real=0.0, imag=0.0):
+
+   """Описание функции complex.
+
+
+
+     Описание аргументов:
+
+     real – описание аргумента
+
+     imag – описание аргумента
+
+
+
+     """
+
+     if imag == 0.0 and real == 0.0: return complex_zero
+
+     ...
